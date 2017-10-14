3f03fdf71fbbb5ac61fbfc3898e18cb34a959fab
nutricare <32693651+nutricare@users.noreply.github.com>
2017-10-14 15:17:46 -0400

    Added jquery , main javascript library
    main javascript library : to hold javascript and jquery functions

---
+++ javascript/main.js
@@ -0,0 +1,14 @@
+
+$(document).ready(function () {
+
+    alert("hi");
+    $("#btn_login").on('click', function () {
+        
+        document.location = "register/index.html";
+        
+    });
+
+
+
+});
+
