5660015854db932cac617a092907e780a3f2a20f
prabhrai <prabhrai94@gmail.com>
2017-10-15 16:36:21 -0400

    JS File functions

--- javascript/main.js
+++ javascript/main.js
@@ -3,11 +3,34 @@ $(document).ready(function () {
 
     alert("hi");
     $("#btn_login").on('click', function () {
-        
+
         document.location = "register/index.html";
-        
+
     });
 
+    $.ajax({
+        type: 'GET',
+        url: 'localhost/nutricare/users.php',
+        success: function (data) {
+            console.log(data + " X");
+        }
+
+
+
+    });
+    console.log("Y");
+
+
+
+    $.getJSON('C:/Users/jkps/Documents/GitHub/nutricare.github.io/nutricare/users.php', function (data) {
+        console.log(data);
+     console.log("y");
+     });
+
+});
+
+
+$(document).ready(function () {
 
 
 });
